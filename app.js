05366aac56814d547a18cfc9379da3196a5d92ee
Rajyavardhan21 <123850906+Rajyavardhan21@users.noreply.github.com>
2023-01-29 10:31:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,78 +1,29 @@
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const router = require('./Routers/router');
-// const mongoose = require('mongoose')
-// const fs = require('fs');
-
-// const hostname = "localhost";
-// const port = 8008;
-
-// const app = express();
-
-// app.use(bodyParser.json()); 
-// app.use((req,res,next)=>{
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET','POST','PUT','PATCH','DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type','Authorization');
-// next();
-// });
-
-// app.get('/video', function(req,res){
-    
-//     const range = req.headers.range;
-//     if(!range){
-//         res.status(400).send("Requires Range Header");
-//     }
-
-//     const videoPath = './Bigbunny.mp4'
-//     const videoSize = fs.statSync('Bigbunny.mp4').size;
-
-//     const CHUNK_SIZE = 10**6;
-//     const start = Number(range.replace(/\D/g, ""));
-//     const end = Math.min(start + CHUNK_SIZE, videoSize-1);
-
-//     const contentLength = end-start+1;
-//     const headers = {
-//         "Content-Range" : `bytes ${start}-${end}/${videoSize}`,
-//         "Accept-Ranges" : "bytes",
-//         "Content-Length" : contentLength,
-//         "Content-Type" :"video/mp4"
-//     };
-    
-//     res.writeHead(206, headers);
-
-//     const videoStream = fs.createReadStream(videoPath, { start, end});
-//     videoStream.pipe(res);
-// });
-
-
-// app.use('/',router)
-// mongoose.set('strictQuery', false);
-// mongoose.connect('mongodb://127.0.0.1:27017/movies_db', {useNewUrlParser:true, useUnifiedTopology:true})
-// .then(client =>{
-//     app.listen(port, hostname, ()=>{
-//         console.log(`Server is running on http://${hostname}:${port}`);
-//     });
-// }).catch(err =>{
-//     console.log(err);
-// })
-
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+
 const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
 
 //connect to MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/movies_db', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(client => {
-        console.log("Connected to MongoDB");
+
+const mongoose = require('mongoose')
+
+const url = 'mongodb+srv://miniprojectfour:me2ciDtJCkbgVCMJ@cluster0.xxiuhto.mongodb.net/movies_db?retryWrites=true&w=majority';
+
+const connectionParams={
+    useNewUrlParser: true,
+   
+    useUnifiedTopology: true 
+}
+mongoose.connect(url,connectionParams)
+    .then( () => {
+        console.log('Connected to the database ')
     })
-    .catch(err => {
-        console.log(err);
-    });
+    .catch( (err) => {
+        console.error(`Error connecting to the database. n${err}`);
+    })
 
 //define movie schema
 const movieSchema = new mongoose.Schema({
@@ -123,38 +74,22 @@ app.get('/movies/:id', (req, res) => {
 
 //stream movie
 app.get('/movies/stream/:id', (req, res) => {
-    const range = req.headers.range;
-    if (!range) {
-        res.status(400).send("Requires Range Header");
-    }
-    else {
+    
     Movie.findOne({ id: req.params.id })
     .then(movie => {
     if (!movie) {
     res.status(404).json({ message: "Movie not found" });
     }
+    
     else {
-    const videoPath = movie.trailer;
-    const videoSize = fs.statSync(videoPath).size;
-    const CHUNK_SIZE = 500 * 1024; // 500KB
-    const start = Number(range.replace(/\D/g, ""));
-    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
-    const contentLength = end - start + 1;
-    const headers = {
-    "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-    "Accept-Ranges": "bytes",
-    "Content-Length": contentLength,
-    "Content-Type": "video/mp4"
-    };
-    res.writeHead(206, headers);
-    const videoStream = fs.createReadStream(videoPath, { start, end });
-    videoStream.pipe(res);
+            res.status(200).json(movie.trailer);
+        
     }
     })
     .catch(err => {
     res.status(500).json({ message: "Error streaming movie", error: err });
     });
-    }
+    
     });
     
     //start server
@@ -163,5 +98,3 @@ app.get('/movies/stream/:id', (req, res) => {
     app.listen(port, hostname, () => {
     console.log(`Server is running on http://${hostname}:${port}`);
     });
-    
-    
